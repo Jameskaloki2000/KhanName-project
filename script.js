2a81ff3259e203786c6995c09b0681526aac3f88
james kaloki <kaloki2012@gmail.com>
2021-02-21 13:25:20 +0300

    logged the username to see if te form is working

---
+++ script.js
@@ -0,0 +1,8 @@
+// let username = document.querySelector(".username").value;
+// document.querySelector(".date").value;
+// document.querySelector(".month").value;
+
+// console.log(username);
+document.querySelector(".button").addEventListener("click", function () {
+  console.log(document.querySelector(".username").value);
+});
